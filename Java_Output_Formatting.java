255e1521bc62e4d3bada8d17ed9df333df6ec8de
NishantSingh <55411341+nishantsingh9084@users.noreply.github.com>
2020-07-19 15:11:08 +0530

    Update Java_Output_Formatting.java

--- Java_Output_Formatting.java
+++ Java_Output_Formatting.java
@@ -47,3 +47,13 @@ public class Solution {
 
     }
 }
+
+
+/*here ("%-15s%03d%n", s1, x) % : used as a formatter.
+'-' : minus sign used for left indentation of the string.
+15s : here 15 denotes the string's minimum field width 15. 
+'0' : pads the extra 0s in the integer.
+3d : here 3 denotes integer's minimum field width 3.
+%n : prints the new line.
+
+Hope, this helps!*/
